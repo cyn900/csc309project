c9a0b9ecc6dc63077441ffcb09945d343a1a7dee
William Lam <williamntlam@gmail.com>
2024-11-02 18:32:44 -0400

    feat: created startup.sh file

---
+++ scriptorium/scripts/startup.sh
@@ -0,0 +1,11 @@
+npm install @prisma/client
+npm install bcrypt
+npm install express
+npm install jsonwebtoken
+npm install multer
+npm install next
+npm install react
+npm install react-dom
+npm install uuid
+
+node ./setupAdmin.js
