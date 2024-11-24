710c03943f5555ca1db96679d1331f5fd44cd573
cynthia <cyn.zhou@mail.utoronto.ca>
2024-11-24 01:52:34 -0500

    add my template on nav bar

--- PP1/pages/components/Navbar.tsx
+++ PP1/pages/components/Navbar.tsx
@@ -215,6 +215,17 @@ const Navbar: React.FC = () => {
                   My Blogs
                 </button>
               </Link>
+              <Link href="/my-templates">
+                <button
+                  className={`block px-4 py-2 text-sm w-full text-left ${
+                    isDarkMode
+                      ? "text-white hover:bg-gray-600"
+                      : "text-gray-700 hover:bg-gray-100"
+                  }`}
+                >
+                  My Templates
+                </button>
+              </Link>
               <button
                 onClick={handleLogout}
                 className={`block px-4 py-2 text-sm w-full text-left ${
